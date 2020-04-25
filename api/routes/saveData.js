37be6f98d0dfc61cf8eaf762c8094d13408afd7f
Andrew Whelan <whelandrew@gmail.com>
2020-04-25 15:11:34 -0700

    all code

---
+++ api/routes/saveData.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+
+
+
+module.exports = router;
---
+++ client/src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {Route, withRouter} from 'react-router-dom';
+
+import auth0Client from './Auth';
+import NavBar from './NavBar/NavBar';
+import Dropbox from './DropboxChooser/DropboxChooser';
+import Carousel from './Carousel/Carousel';
+import Callback from './Callback/Callback';
+
+class App extends React.Component 
+{
+	constructor(props) 
+	{
+		super(props);
+		this.state = 
+		{
+			checkingSession: true,
+		}
+	}
+  
+	async componentDidMount() 
+	{
+		if (this.props.location.pathname === '/Callback') 
+		{
+			this.setState({checkingSession:false});
+			return;
+		}
+		try 
+		{
+			await auth0Client.silentAuth();
+			this.forceUpdate();
+		} 
+		catch (err) 
+		{ 
+			if (err.error !== 'login_required') 
+			{
+				console.log(err.error);
+			}
+		}
+	}
+	
+  render() {
+    return (
+      <div>
+		<NavBar />
+			<div>
+			{	auth0Client.isAuthenticated() 
+				&& 	<Route exact path='/' component={Dropbox}/>
+			}		
+			<Route exact path='/Callback' component={Callback}/>
+			<Route exact path='/Carousel' component={Carousel}/>
+		</div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(App);
