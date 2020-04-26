614f1898f471d5ecb52532606b262df0cb9a3eae
Andrew Whelan <whelandrew@gmail.com>
2020-04-25 17:11:31 -0700

    Heroku updates

---
+++ app.js
@@ -0,0 +1,21 @@
+{
+  "name": "Kroms Image Slider",
+  "description": "",
+  "scripts": {
+  },
+  "env": {
+  },
+  "formation": {
+    "web": {
+      "quantity": 1
+    }
+  },
+  "addons": [
+
+  ],
+  "buildpacks": [
+    {
+      "url": "heroku/nodejs"
+    }
+  ]
+}
